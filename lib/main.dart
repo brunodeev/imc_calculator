90293fe9d5cb3d4017a6f2c70af365ce68dd91c3
Bruno César <bcgmeireles@gmail.com>
2022-09-24 08:35:11 -0300

    criação do projeto

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Calculadora de IMC'),
+      ),
+    );
+  }
+}
